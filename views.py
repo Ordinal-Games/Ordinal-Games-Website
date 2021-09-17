ad2810f1981261027d15e98ad68aad01c9d64b42
TriangleDevv <chandu.lanka13@gmail.com>
2021-09-17 11:36:58 +0530

    got all the routes finished all that is left is to make content for each route #almostdonebois

--- views.py
+++ views.py
@@ -2,6 +2,24 @@ from flask import Blueprint, render_template
 
 views = Blueprint(__name__, "views")
 
+@views.route('/home')
 @views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/about')
+@views.route('/team')
+def team():
+    return render_template("team.html")
+
+@views.route('/software')
+@views.route('/game')
+@views.route('/games')
+def games():
+    return render_template("games.html")
+
+@views.route('/contact')
+@views.route('/contacts')
+@views.route('/contact-us')
+def contact():
+    return render_template("contact.html")
